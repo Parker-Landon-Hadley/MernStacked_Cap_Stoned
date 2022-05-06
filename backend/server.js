eab071961f852de92c02e45a92c7fa1b54f0df5a
ParkerHadley <parker.l.hadley@gmail.com>
2022-05-06 08:04:29 -0400

    Able to GET product list through postman and on browser

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import data from './data.js';
 
 const app = express();
 
-app.get('/api/products',)
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+});
+
+const port = process.env.PORT || 3030;
+app.listen(port, () => {
+    console.log(`welcome to http://localhost:${port}`);
+});
