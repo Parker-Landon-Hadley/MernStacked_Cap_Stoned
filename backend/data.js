62e5d86fb708166f01e0c7bc411f0043c066ee25
ParkerHadley <parker.l.hadley@gmail.com>
2022-05-06 07:41:00 -0400

    Created data.js and test inventory

---
+++ backend/data.js
@@ -0,0 +1,51 @@
+const data = {
+    products: [
+        {
+            brand: 'White Faced Bear',
+            name: 'The First Fire',
+            category: 'Hot Sauces',
+            price: 12.99,
+            numberInStock: 22,
+            rating: 4.7,
+            numReviews: 30,
+            description: 'Sauce made from ghost peppers and volcanic ash'
+            
+        },
+        {
+            brand: 'White Faced Bear',
+            name: 'Water Famine',
+            category: 'Hot Sauces',
+            price: 12.99,
+            numberInStock: 22,
+            rating: 4.7,
+            numReviews: 30,
+            description: 'Sauce made from ghost peppers and volcanic ash',
+            
+        },
+        {
+            brand: 'White Faced Bear',
+            name: 'Sun Dance',
+            category: 'BBQ Sauces',
+            price: 12.99,
+            numberInStock: 22,
+            rating: 4.7,
+            numReviews: 30,
+            description: 'Sauce made from ghost peppers and volcanic ash',
+            
+        },
+        {
+            brand: 'White Faced Bear',
+            name: 'Rainbows End',
+            category: 'BBQ Sauces',
+            price: 12.99,
+            numberInStock: 22,
+            rating: 4.7,
+            numReviews: 30,
+            description: 'Sauce made from ghost peppers and volcanic ash',
+            
+        },
+    ]
+};
+
+export default data;
+
