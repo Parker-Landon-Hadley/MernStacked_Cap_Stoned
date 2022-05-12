ecad3bf29b5ff7d6eb25f4bcc770c1974cce1e0f
ParkerHadley <parker.l.hadley@gmail.com>
2022-05-12 07:17:35 -0400

    Linked frontend to backend api, installed axios

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
 
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3003;
 app.listen(port, () => {
     console.log(`welcome to http://localhost:${port}`);
 });
