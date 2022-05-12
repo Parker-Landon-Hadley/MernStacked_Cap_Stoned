396ce3ad086a633af3e403b872ccf4b81222d528
ParkerHadley <parker.l.hadley@gmail.com>
2022-05-12 07:48:21 -0400

    Successfully fetched data from backend api and rendered in front end

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,10 @@ import data from './data.js';
 const app = express();
 
 app.get('/api/products', (req, res) => {
-    res.send(data.products);
+  res.send(data.products);
 });
 
 const port = process.env.PORT || 3003;
 app.listen(port, () => {
-    console.log(`welcome to http://localhost:${port}`);
+  console.log(`welcome to http://localhost:${port}`);
 });
