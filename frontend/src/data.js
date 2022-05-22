7f5712547000a0169aab3449a0ebe2a7574cbab0
ParkerHadley <parker.l.hadley@gmail.com>
2022-05-22 10:48:13 -0400

    fixed frontend so it shows on github, added components for product and rating, added fontawesome for rating star visual

---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      brand: 'White Faced Bear',
+      name: 'The First Fire',
+      category: 'Hot Sauces',
+      image: '/images/p1.jpg', // 679px × 829px
+      price: 12.99,
+      numberInStock: 29,
+      rating: 4.7,
+      numReviews: 32,
+      description: 'Sauce made from ghost peppers and volcanic ash',
+      ingredients: 'Habanero, Tomato, vinegar, salt, water',
+    },
+    {
+      brand: 'White Faced Bear',
+      name: 'Water Famine',
+      category: 'Hot Sauces',
+      image: '/images/p2.jpg',
+      price: 9.99,
+      numberInStock: 14,
+      rating: 4.1,
+      numReviews: 17,
+      description: 'Sauce made from ghost peppers and volcanic ash',
+      ingredients: '',
+    },
+    {
+      brand: 'White Faced Bear',
+      name: 'Sun Dance',
+      category: 'Sauerkraut',
+      image: '/images/p3.jpg',
+      price: 10.25,
+      numberInStock: 22,
+      rating: 3.8,
+      numReviews: 8,
+      description: 'Sauce made from ghost peppers and volcanic ash',
+      ingredients: '',
+    },
+    // {
+    //   brand: 'White Faced Bear',
+    //   name: 'Rainbows End',
+    //   category: 'BBQ Sauces',
+    //   image: '/images/p4.jpg',
+    //   price: 16.37,
+    //   numberInStock: 10,
+    //   rating: 4.5,
+    //   numReviews: 30,
+    //   description: 'Sauce made from ghost peppers and volcanic ash',
+    //   ingredients: '',
+    // },
+  ],
+};
+
+export default data;
