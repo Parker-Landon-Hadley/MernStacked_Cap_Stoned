066a17382a172e2f98738f407d60e0efaa29f2a9
ParkerHadley <parker.l.hadley@gmail.com>
2022-05-24 06:52:01 -0400

    create store.js and reducer for addtocartitem

--- frontend/src/pages/ProductScreen.js
+++ frontend/src/pages/ProductScreen.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useReducer } from 'react';
+import { useContext, useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -9,6 +9,8 @@ import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 import Rating from '../components/Rating';
 import { Helmet } from 'react-helmet-async';
+import { Store } from '../Store';
+
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -46,6 +48,15 @@ function ProductScreen() {
 
     fetchData();
   }, [slug]);
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const addToCartHandler = () => {
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...product, quantity: 1 },
+    });
+  };
+
   return loading ? (
     <div>Loading...</div>
   ) : error ? (
@@ -107,7 +118,7 @@ function ProductScreen() {
                 {product.numInStock > 0 && (
                   <ListGroup.Item>
                     <div className="d-grid">
-                      <Button variant="primary">
+                      <Button onClick={addToCartHandler} variant="primary">
                         Add to Cart
                       </Button>
                     </div>
